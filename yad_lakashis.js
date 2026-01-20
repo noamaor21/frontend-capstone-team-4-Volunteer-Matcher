521944bdc538f1f60431e2f0b9213f5331690874
gili amsalem <giliam456@gmail.com>
2026-01-20 23:17:16 +0200

    Add navbar to Organizations and Yad LaKashish pages

--- yad_lakashis.js
+++ yad_lakashis.js
@@ -4,21 +4,89 @@ var buttons = document.querySelectorAll(".volunteer-btn");
 var organization = document.getElementById("org-name").innerText;
 
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function()
-    {
-        
+    buttons[i].addEventListener("click", function () {
+
         var userName = localStorage.getItem("user_full_name");
 
         if (userName === null) {
             alert("עליך להירשם באתר לפני שתוכל להירשם להתנדבות!");
             window.location.href = "form.html";
-        } 
+        }
         else {
             localStorage.setItem("chosen_org", organization);
-            
+
             alert(userName + ", נרשמת בהצלחה ל" + organization);
             window.location.href = "personal_area.html";
         }
     }
-);
+    );
+}
+
+
+let hamburgerButton = document.getElementById('hamburger');
+let menu = document.getElementById('menu');
+let menuIcon = document.querySelector('.menuIcon');
+let closeIcon = document.querySelector('.closeIcon');
+
+if (hamburgerButton && menu && menuIcon && closeIcon) {
+  hamburgerButton.addEventListener('click', function () {
+    menu.classList.toggle('showMenu');
+
+    if (menu.classList.contains('showMenu')) {
+      menuIcon.style.display = 'none';
+      closeIcon.style.display = 'inline';
+    } else {
+      menuIcon.style.display = 'inline';
+      closeIcon.style.display = 'none';
+    }
+  });
+
+
+  let menuLinks = document.querySelectorAll('.nav-list a');
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      menu.classList.remove('showMenu');
+      menuIcon.style.display = 'inline';
+      closeIcon.style.display = 'none';
+    });
+  });
+}
+
+
+
+
+let darkModeToggle = document.getElementById('dark-mode-toggle');
+let iconMoon = document.getElementById('icon-moon');
+let iconSun = document.getElementById('icon-sun');
+
+function updateThemeIcon() {
+  if (!iconMoon || !iconSun) return;
+
+  if (document.body.classList.contains('dark-mode')) {
+    iconMoon.style.display = 'none';
+    iconSun.style.display = 'block';
+  } else {
+    iconMoon.style.display = 'block';
+    iconSun.style.display = 'none';
+  }
+}
+
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark-mode');
+}
+updateThemeIcon();
+
+if (darkModeToggle) {
+  darkModeToggle.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('theme', 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
+
+    updateThemeIcon();
+  });
 }
